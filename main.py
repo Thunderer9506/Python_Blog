83a65612894e997fd09f97facd4876b0d3825566
Shauyra Srivastava <srivastavashaurya225@gmail.com>
2023-09-28 22:56:14 +0530

    some changes

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from forms import CreatePostForm,RegisterForm,LoginForm,CommentForm
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
