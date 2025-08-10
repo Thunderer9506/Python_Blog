646285c7d263d8dd608b714af8d309505cdfdb45
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-08-10 23:08:08 +0530

    changed name of submit buttons

--- forms.py
+++ forms.py
@@ -17,15 +17,15 @@ class RegisterForm(FlaskForm):
     email = StringField("Email",validators=[DataRequired()])
     password = StringField("Password",validators=[DataRequired()])
     name = StringField("Name",validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
+    submit = SubmitField("Register")
 
 # TODO: Create a LoginForm to login existing users
 class LoginForm(FlaskForm):
     email = StringField("Email",validators=[DataRequired()])
     password = StringField("Password",validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
+    submit = SubmitField("Login")
 
 # TODO: Create a CommentForm so users can leave comments below post
 class CommentForm(FlaskForm):
     body = CKEditorField("Blog Content", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
+    submit = SubmitField("Comment")
